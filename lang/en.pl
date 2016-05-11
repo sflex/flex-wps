a919b93945c73203e72248c15b2eeb8d7bd22aa7
sflex <shakaflex@gmail.com>
2016-05-11 13:36:12 -0400

    Add files via upload

---
+++ lang/en.pl
@@ -0,0 +1,51 @@
+=head1 COPYLEFT
+
+ en.pl, v1.00 04/13/2009 09:31:05 By: N.K.A.
+
+ English file for Flex-wps
+
+=cut
+
+%adm = (
+site_adm => 'Site Administration',
+welcome_msg => 'Welcome Message',
+main_menu => 'Main Menu',
+user_menu => 'User Menu',
+
+);
+
+%months = (
+                   0 => 'January',
+                   1 => 'February',
+                  10 => 'November',
+                  11 => 'December',
+                   2 => 'March',
+                   3 => 'April',
+                   4 => 'May',
+                   5 => 'June',
+                   6 => 'July',
+                   7 => 'August',
+                   8 => 'September',
+                   9 => 'October',
+);
+
+%week_days = (
+                   0 => 'Sunday',
+                   1 => 'Monday',
+                   2 => 'Tuesday',
+                   3 => 'Wednesday',
+                   4 => 'Thursday',
+                   5 => 'Friday',
+                   6 => 'Saturday',
+);
+
+%usr = (
+ admin => 'Administrator',
+ mod => 'Moderator',
+ user => 'User',
+ anonuser => 'Guest',
+ sadmin => 'admin',
+ sfadmin => 'Admin',
+ );
+ 
+1;
